018cb326acfddedb7da9457e36513389b2b523bc
Romain Goldenchtein <romain.goldenchtein@utt.fr>
2024-11-20 17:53:56 +0100

    Ajout SPEC1

---
+++ src/roomSearch.js
@@ -0,0 +1,36 @@
+const { processCruData } = require('./controller');
+const readline = require('readline');
+const path = require('path');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const rootPath = path.resolve(__dirname, '../data'); // Dossier contenant les sous-dossiers .cru
+const summary = processCruData(rootPath);
+
+function searchRoomsForCourse(courseCode) {
+    if (!summary[courseCode]) {
+        console.log("Le cours est introuvable. Veuillez vérifier le code du cours.");
+        return;
+    }
+
+    const rooms = summary[courseCode].rooms;
+    if (rooms.length === 0) {
+        console.log("Aucune salle n'a pu être trouvée pour ce cours.");
+    } else {
+        console.log("Salles associées au cours " + courseCode + " :");
+        console.log(rooms.join('\n'));
+    }
+}
+
+function promptCourseCode() {
+    rl.question("Veuillez entrer le code du cours : ", (courseCode) => {
+        searchRoomsForCourse(courseCode);
+        rl.close();
+    });
+}
+
+console.log("Recherche des salles associées à un cours");
+promptCourseCode();
