c9b5b5de2d9683c2377feacf1f30c2405e040459
6666mon <simon.gelbart@utt.fr>
2024-12-03 20:03:22 +0100

    rajout de la gestion d'erreur au lieu de trier par défaut si on se trompe d'entree pour le tri

--- src/roomOccupancyRate.js
+++ src/roomOccupancyRate.js
@@ -11,17 +11,34 @@ async function promptRoomOccupancy(rl) {
     const answer = await promptUser("Souhaitez-vous calculer le taux d'occupation pour toutes les salles ("+ "O".green+ "/"+ "N".red+") ? ", rl);
     if (answer.toUpperCase() === 'O') {
         const occupancyRates = calculateOccupancyRateForAllRooms();
-
+        
         if (occupancyRates.error) {
             console.log(occupancyRates.error.red);
-        } else {
-            console.log("Taux d'occupation des salles trié par nombre nombre de places".inverse);
+        }else{
+            let sortChoice='';
+            do{
+            sortChoice = await promptUser("Voulez-vous trier par taux d'occupation (1) ou par capacité (2) ? ", rl);
+            if (sortChoice !== '1' && sortChoice !== '2') {
+                console.log("Choix invalide. Veuillez choisir 1 pour trier par taux d'occupation ou 2 pour trier par capacité.".red);
+            }
+        }while(sortChoice !== '1' && sortChoice !== '2');
+            if (sortChoice==='1'){
+                occupancyRates.sort((a, b) => b.occupancyRate - a.occupancyRate);
+                console.log("Taux d'occupation des salles trié par taux d'occupation :".inverse);
+            } else if (sortChoice === '2') {
+                occupancyRates.sort((a, b) => b.capacity - a.capacity);
+                console.log("Taux d'occupation des salles trié par capacité :".inverse);
+            } else {
+                console.log("Choix invalide. Tri par taux d'occupation par défaut.".yellow);
+                occupancyRates.sort((a, b) => b.occupancyRate - a.occupancyRate);
+            }
+
             occupancyRates.forEach(({ room, occupancyRate, capacity }) => {
                 if (room.trim() !== "") {
-                    console.log(`Salle ${room.brightCyan} : Capacité de ${capacity.toString().brightGreen} personnes pour un taux d'occupation de ${occupancyRate.toFixed(2).toString().brightGreen + '%'.brightGreen}`);
+                    console.log(`Salle ${room.brightCyan} : ${occupancyRate.toFixed(2).toString().brightGreen +'%'.brightGreen} d'occupation et Capacité de ${capacity.toString().brightGreen} personnes`);
                 }
             });
-        }
+        } 
     } else {
         console.log("Opération annulée.".yellow);
     }
